4cfb86e49eb5c140211ee1de2bac8dd1c34a6b1b
Natali Likhachova <nataliya.likhachova@gmail.com>
2019-01-17 23:17:46 +0200

    added blog

--- app/js/js.js
+++ app/js/js.js
@@ -1,10 +1,27 @@
-$('.top__carousel').slick();
+$('.top__carousel').slick(
+    responsive [
+        {
+            breakpoint: 600,
+            settings:{
+                arrows: false
+            }
+        }
+        ]
+);
 
 $('.testimonials__carousel').slick({
     dots: true,
     arrows: false,
     autoplay: true,
-    autoplaySpeed: 3000
+    autoplaySpeed: 3000,
+    responsive: [
+        {
+            breakpoint: 600,
+            settings:{
+                dots: false
+            }
+        }
+    ]
 });
 
 
